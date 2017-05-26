827f83796a7ee0efedc54aad668180466f736165
Patrick Lewis <PatrickDanielLewis@gmail.com>
2017-05-26 01:41:35 +0000

    added photo verifcation to the test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,7 @@ RSpec.configure do |config|
   
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+   include ActionDispatch::TestProcess
   
 end
 
